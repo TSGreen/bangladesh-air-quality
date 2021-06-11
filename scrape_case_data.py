b53ebef3ac12007a198faaa98d77c2dd59f0d312
TSGreen <tim.green2011@gmail.com>
2021-06-11 18:06:54 +0600

    Updated scraper to account for date of previous scrape.

--- scrape_case_data.py
+++ scrape_case_data.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Script for web scraper to crawl through CASE (Govt of Bangladesh) data archive.
 
@@ -7,20 +5,35 @@ Data archive found at: https://bit.ly/31t9vxr
 Leading to html table of a day's data (for example: https://bit.ly/3a5DYWc)
 
 Data is avaiable from February 2014, but is not exhaustive, with some dates
-missing data. Also different dates have different locations avaible.
+missing data. Also different dates have different locations available.
 Care needed when concatenating the daily figures.
 
 Script returns a dataframe version of each day's data.
 
+Updated (06/21) to only scrape dates after the most scrapping date.
+
 Created August 2020.
-@author: tim
+Updated June 2021
+@author: Timothy Green
+
 """
 
 import scrapy
 from scrapy.crawler import CrawlerProcess
 import pandas as pd
 from pathlib import Path
-import os 
+from datetime import datetime
+import re
+
+
+def return_formated_date(date):
+    """Return date string as datetime."""
+    date = date.split('/')
+    if len(date[2])==2:
+        date[2] = '20'+date[2]
+    date = '/'.join(date)
+    return datetime.strptime(date, '%d/%m/%Y')
+
 
 class CaseSpider(scrapy.Spider):
     """
@@ -42,12 +55,19 @@ class CaseSpider(scrapy.Spider):
         """First parsing to retrieve link to each daily data page."""
         html_blocks = response.xpath('//a[contains(@href,"aqi-archives")]')
         links_to_follow = html_blocks.xpath('@href').extract()
-        dates = html_blocks.xpath('text()').extract()
-        for url, date in zip(links_to_follow, dates):
-            date = date.strip()[24:]
-            yield response.follow(url=url,
-                                  callback=self.parse_pages,
-                                  meta={'date': date})
+        descriptions = html_blocks.xpath('text()').extract()
+        for url, text in zip(links_to_follow, descriptions):
+            date_list = re.findall('[0-9]+/[0-9]+/[0-9]+', text)
+            if date_list:
+                date = date_list[0]
+            else:
+                date = re.findall('[0-9]+.[0-9]+.[0-9]+', text)[0]
+                date = date.replace('.', '/')
+            date_formatted = return_formated_date(date)
+            if date_formatted > last_scraped:
+                yield response.follow(url=url,
+                                      callback=self.parse_pages,
+                                      meta={'date': date})
 
     def parse_pages(self, response):
         """Second parsing to extract data table and convert to dataframe."""
@@ -63,9 +83,14 @@ class CaseSpider(scrapy.Spider):
 
 url = "http://case.doe.gov.bd/index.php?option=com_xmap&sitemap=1&Itemid=14"
 
-datafile = Path.cwd().joinpath('data', 'case_data.csv')
+datafile = Path.cwd().joinpath('data', 'bronze', 'case', 'case_data.csv')
 if datafile.exists():
-    os.remove(datafile)
+    df = pd.read_csv(datafile)
+    df['Date'] = pd.to_datetime(df['Date'], 
+                                errors='coerce', 
+                                dayfirst=True)
+    last_scraped = df.Date.max()
+    #os.remove(datafile)
 
 # Run the Spider
 process = CrawlerProcess()
